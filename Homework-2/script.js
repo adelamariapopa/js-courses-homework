29167d08c60602312108463d4b6dd957c459daf6
adelamariapopa <adela.popa@softvision.com>
2021-05-18 14:32:34 +0300

    identation css

--- Homework-2/script.js
+++ Homework-2/script.js
@@ -5,13 +5,19 @@ const resetButton = document.querySelector('#reset-stopwatch');
 let timer;
 let millisecond = 0;
 function startWatch() {
+  // if(timer !== '59 : 59 : 59'){
+  //   console.log('diferit');
+  // }
   clearInterval(timer);
   timer = setInterval( ()=> {
     millisecond +=10;
-    let date = new Date(millisecond);
-    let milliseconds = String(date.getMilliseconds()).split(0,1);
-    let seconds = String(date.getSeconds()).padStart(2, "0");
-    let minutes = String(date.getMinutes()).padStart(2, "0");
+    let date;
+    let milliseconds = String(date.setMilliseconds('59')).split(0,1);
+    let seconds = String(date.setSeconds('59')).padStart(2, "0");
+    let minutes = String(date.setMinutes('59')).padStart(2, "0");
+    // let milliseconds = String(date.getMilliseconds()).split(0,1);
+    // let seconds = String(date.getSeconds()).padStart(2, "0");
+    // let minutes = String(date.getMinutes()).padStart(2, "0");
     stopWatch.innerHTML = `${minutes} : ${seconds} : ${milliseconds}`;
   },10);
   startButton.classList.toggle('pause-btn');
