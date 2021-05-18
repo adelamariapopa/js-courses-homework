a2eb04d0322a5a8f1556eff0ee724cf633b5903f
adelamariapopa <adela.popa@softvision.com>
2021-05-18 14:34:29 +0300

    js indentation

--- Homework-2/script.js
+++ Homework-2/script.js
@@ -5,25 +5,20 @@ const resetButton = document.querySelector('#reset-stopwatch');
 let timer;
 let millisecond = 0;
 function startWatch() {
-  // if(timer !== '59 : 59 : 59'){
-  //   console.log('diferit');
-  // }
   clearInterval(timer);
   timer = setInterval( ()=> {
-    millisecond +=10;
+    millisecond += 10;
     let date;
-    let milliseconds = String(date.setMilliseconds('59')).split(0,1);
-    let seconds = String(date.setSeconds('59')).padStart(2, "0");
-    let minutes = String(date.setMinutes('59')).padStart(2, "0");
-    // let milliseconds = String(date.getMilliseconds()).split(0,1);
-    // let seconds = String(date.getSeconds()).padStart(2, "0");
-    // let minutes = String(date.getMinutes()).padStart(2, "0");
+    let milliseconds = date.setMilliseconds
+    let milliseconds = String(date.getMilliseconds()).split(0,1);
+    let seconds = String(date.getSeconds()).padStart(2, "0");
+    let minutes = String(date.getMinutes()).padStart(2, "0");
     stopWatch.innerHTML = `${minutes} : ${seconds} : ${milliseconds}`;
-  },10);
+  }, 10);
   startButton.classList.toggle('pause-btn');
-  if(startButton.classList.contains('pause-btn')){
+  if(startButton.classList.contains('pause-btn')) {
     startButton.textContent = 'stop';
-  }else{
+  } else {
     startButton.textContent = 'play';
     clearInterval(timer);
   }
